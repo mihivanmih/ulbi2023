7ee55a10fa9d686e7b68fd92400f191fd1447d77
Gusb <mihivanmih@gmail.com>
2023-09-20 15:58:37 +0300

    lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Suspense } from "react";
+import {Suspense, useContext, useState} from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
-            App
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Сменить тему</button>
             <Counter />
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
