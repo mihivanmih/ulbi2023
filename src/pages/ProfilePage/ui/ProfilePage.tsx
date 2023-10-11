8eb769fb88a97df8b4ccbde3e440d19eaa99979d
Gusb <mihivanmih@gmail.com>
2023-10-12 02:05:16 +0300

    lesson 39

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -2,7 +2,9 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { useTranslation } from 'react-i18next'
 import type { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
 import { DynamicModuleLoader } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
-import { profileReducer } from 'entities/Profile'
+import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile'
+import { useEffect } from 'react'
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch'
 
 const reducers: ReducersList = {
     profile: profileReducer
@@ -14,10 +16,16 @@ interface ProfilePageProps {
 const ProfilePage = (props: ProfilePageProps) => {
     const { t } = useTranslation()
 
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchProfileData())
+    }, [dispatch])
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [])}>
-                {t('PROFILE pAGE')}000
+                <ProfileCard />
             </div>
         </DynamicModuleLoader>
     )
