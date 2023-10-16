e9f2db650326b6d184d7dbb06f3b3a6e1d3aa665
Gusb <mihivanmih@gmail.com>
2023-10-16 20:34:55 +0300

    lesson 42

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,4 +1,5 @@
 export type { ProfileSchema, Profile } from './model/types/profile'
+export { ValidateProfileError } from './model/types/profile'
 export { profileReducer, profileActions } from './model/slice/profileSlice'
 export { fetchProfileData } from './model/services/fetchProfileData/fetchProfileData'
 export { updateProfileData } from './model/services/updateProfileData/updateProfileData'
@@ -9,3 +10,4 @@ export { getProfileIsLoading } from './model/selectors/getProfileIsLoading/getPr
 export { getProfileFirstData } from './model/selectors/getProfileFirstData/getProfileFirstData'
 export { getProfileReadonly } from './model/selectors/getProfileReadonly/getProfileReadonly'
 export { getProfileForm } from './model/selectors/getProfileForm/getProfileForm'
+export { getProfileValidateErrors } from './model/selectors/getProfileValidateErrors/getProfileValidateErrors'
