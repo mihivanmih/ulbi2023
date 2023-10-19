82677ab3807a8666f95f183c8c1dfa15f86084f5
Gusb <mihivanmih@gmail.com>
2023-10-19 18:06:18 +0300

    lesson 53

---
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.ts
@@ -0,0 +1,44 @@
+import { createSelector } from '@reduxjs/toolkit'
+import { getUserAuthData } from 'entities/User'
+import { RoutePath } from 'shared/config/roteConfig/routeConfig'
+import MainIcon from 'shared/assets/icons/main20x20.svg'
+import AboutIcon from 'shared/assets/icons/about20x20.svg'
+import ProfileIcon from 'shared/assets/icons/profile-20x20.svg'
+import Articles from 'shared/assets/icons/article-20-20.svg'
+import type { SideBarItemType } from '../types/sidebar'
+
+export const getSidebarItems = createSelector(
+    getUserAuthData, (userData) => {
+        const sidebarItemsList: SideBarItemType[] = [
+            {
+                path: RoutePath.main,
+                Icon: MainIcon,
+                text: 'Главная'
+            },
+            {
+                path: RoutePath.about,
+                Icon: AboutIcon,
+                text: 'О сайте'
+            }
+        ]
+
+        if (userData) {
+            sidebarItemsList.push(
+                {
+                    path: RoutePath.profile + userData.id,
+                    Icon: ProfileIcon,
+                    text: 'Профиль',
+                    authOnly: true
+                },
+                {
+                    path: RoutePath.articles,
+                    Icon: Articles,
+                    text: 'Статьи',
+                    authOnly: true
+                }
+            )
+        }
+
+        return sidebarItemsList
+    }
+)
