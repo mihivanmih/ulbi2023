3fe558a20cceacdde6793d7c29909ee9fdbafb27
Gusb <mihivanmih@gmail.com>
2023-10-23 17:25:14 +0300

    lesson 55

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,55 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import styles from './ARticleViewSelector.module.scss'
+import { memo } from 'react'
+import { ArticleView } from '../../model/types/article'
+import ListIcon from 'shared/assets/icons/list-24-24.svg'
+import TiledIcon from 'shared/assets/icons/tiled-24-24.svg'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
+import { Icon } from 'shared/ui/Icon/Icon'
+
+interface ArticleViewSelectorProps {
+    className?: string
+    view: ArticleView
+    onViewClick: (view: ArticleView) => void
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.SMALL,
+        icon: TiledIcon
+    },
+
+    {
+        view: ArticleView.BIG,
+        icon: ListIcon
+    }
+]
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+    const {
+        className = '',
+        view,
+        onViewClick
+    } = props
+
+    const onClick = (newView: ArticleView) => () => {
+        onViewClick?.(newView)
+    }
+
+    return (
+        <div className={classNames(styles.ArticleViewSelector, {}, [className])}>
+            {viewTypes.map(viewType => (
+                <Button
+                    theme={ThemeButton.CLEAR}
+                    key={viewType.view}
+                    onClick={onClick(viewType.view)}
+                >
+                    <Icon
+                        Svg={viewType.icon}
+                        className={classNames('', { [styles.notSelected]: viewType.view !== view })}
+                    />
+                </Button>
+            ))}
+        </div>
+    )
+})
