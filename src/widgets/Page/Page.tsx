61114293e455ce3104372acdf41b3d34f065cf36
Gusb <mihivanmih@gmail.com>
2023-10-26 16:38:38 +0300

    lesson 63

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -17,6 +17,8 @@ interface PageProps {
     onScrollEnd?: () => void
 }
 
+export const PAGE_ID = 'PAGE_ID'
+
 export const Page = (props: PageProps) => {
     const {
         className = '',
@@ -52,6 +54,7 @@ export const Page = (props: PageProps) => {
             className={classNames(styles.Page, {}, [className])}
             ref={wrapperRef}
             onScroll={onScroll}
+            id={PAGE_ID}
         >
             {children}
             {onScrollEnd ? <div ref={triggerRef} className={styles.trigger}/> : null}
