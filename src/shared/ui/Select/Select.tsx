1ed38f14335210f2861f65acd2d5633e1b9325aa
Gusb <mihivanmih@gmail.com>
2023-11-07 15:23:53 +0300

    lesson 100

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -2,23 +2,23 @@ import type { Mods } from '@/shared/lib/classNames/classNames'
 import { classNames } from '@/shared/lib/classNames/classNames'
 import styles from './Select.module.scss'
 import type { ChangeEvent } from 'react'
-import { memo, useMemo } from 'react'
+import { useMemo } from 'react'
 
-export interface SelectOption {
-    value: string
+export interface SelectOption<T extends string> {
+    value: T
     content: string
 }
 
-interface SelectProps {
+interface SelectProps<T extends string> {
     className?: string
     label?: string
-    options?: SelectOption[]
-    value?: string
-    onChange?: (value: string) => void
+    options?: Array<SelectOption<T>>
+    value?: T
+    onChange?: (value: T) => void
     readonly?: boolean
 }
 
-export const Select = memo((props: SelectProps) => {
+export const Select = <T extends string>(props: SelectProps<T>) => {
     const {
         className = '',
         label,
@@ -30,7 +30,7 @@ export const Select = memo((props: SelectProps) => {
 
     const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
         if (onChange) {
-            onChange(e.target.value)
+            onChange(e.target.value as T)
         }
     }
 
@@ -52,4 +52,4 @@ export const Select = memo((props: SelectProps) => {
             </select>
         </div>
     )
-})
+}
