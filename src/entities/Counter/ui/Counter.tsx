56a0b91c5ebf1d4af544f125fc9172f918cc4c12
Gusb <mihivanmih@gmail.com>
2023-11-07 18:33:05 +0300

    lesson 105

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,28 +1,34 @@
 import { Button } from '@/shared/ui/Button'
-import { useDispatch, useSelector } from 'react-redux'
-import { counterActions } from '../model/slice/counterSlice'
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue'
+import { useCounterActions } from '../model/slice/counterSlice'
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue'
 
 export const Counter = () => {
-    const dispatch = useDispatch()
-    const counterValue = useSelector(getCounterValue)
+    const counterValue = useCounterValue()
+    const { decrement, increment, add } = useCounterActions()
 
-    const increment = () => {
-        dispatch(counterActions.increment())
+    const handleInc = () => {
+        increment()
     }
 
-    const decrement = () => {
-        dispatch(counterActions.decrement())
+    const handleDec = () => {
+        decrement()
+    }
+
+    const handleAddFive = () => {
+        add(5)
     }
 
     return (
         <div>
             <h1 data-testid="value-title">{ counterValue }</h1>
-            <Button onClick={increment} data-testid="increment-btn">
+            <Button onClick={handleInc} data-testid="increment-btn">
                 +
             </Button>
-            <Button onClick={decrement} data-testid="decrement-btn">
+            <Button onClick={handleDec} data-testid="decrement-btn">
                 -
+            </Button>
+            <Button onClick={handleAddFive} data-testid="decrement-btn">
+                + 5
             </Button>
         </div>
     )
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { BugButton } from '@/widgets/PageError'
 import { Page } from '@/widgets/Page'
+import { Counter } from '@/entities/Counter'
 
 const MyComponent = () => {
     const { t } = useTranslation()
@@ -16,6 +17,7 @@ const MyComponent = () => {
             <div>sdsdsd</div>
             <div>sdsdsd</div>
             <div>sdsdsd</div>
+            <Counter />
         </Page>
     )
 }
